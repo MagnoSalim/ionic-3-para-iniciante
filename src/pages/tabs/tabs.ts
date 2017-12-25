8940d594ce9702a31ed60c15a1082f4ccbec0954
MagnoSalim <salim.magno@gmail.com>
2017-12-25 14:55:28 -0200

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,17 @@
+import { FeedPage } from '../feed/feed';
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab4Root = FeedPage;
+
+  constructor() {
+
+  }
+}
